ebf1f7b7351c5be3dbff5ff745ca500b3b711588
Michal Filippi <michal.filippi@sizmek.com>
2019-04-16 15:24:05 +0200

    Setuptools install script

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="previewgen",
+    version="0.1",
+    author="Michal Filippi",
+    author_email="michal.filippi@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/michalfilippi/PreviewGen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'preview-gen = previewgen.__main__:main'
+        ]
+    }
+)
